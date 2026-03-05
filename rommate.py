4e458879762ad447f1bf9ce066eafbf96f136c11
Rodrigo Chaicowski <rchaicowski@gmail.com>
2026-03-04 22:36:39 -0300

    chore: add customtkinter init and fix entry point

--- rommate.py
+++ rommate.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 """
 RomMate - ROM companion tool
+Convert disc images to CHD, create M3U playlists, and verify ROMs.
 Copyright (C) 2026 Rodrigo
 
 This program is free software: you can redistribute it and/or modify
@@ -9,26 +12,24 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 """
 
-#!/usr/bin/env python3
-"""
-RomMate - Your ROM companion
-Convert disc images to CHD and create M3U playlists
-"""
-
 import tkinter as tk
+import customtkinter as ctk
 from gui.main_window import RomMateGUI
 
 
 if __name__ == "__main__":
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
     try:
         from tkinterdnd2 import TkinterDnD
         root = TkinterDnD.Tk()
     except ImportError:
         root = tk.Tk()
-    
+
     app = RomMateGUI(root)
     root.mainloop()
